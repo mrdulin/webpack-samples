69ecc65df11ae36fe9c5fa0bedf67e4da276a399
mrdulin <novaline@qq.com>
2017-11-14 19:38:54 +0800

    update

--- webpack1.x/transform-runtime/webpack.config.js
+++ webpack1.x/transform-runtime/webpack.config.js
@@ -7,33 +7,35 @@ const src = path.join(__dirname, 'src');
 const dist = path.join(__dirname, 'dist');
 
 module.exports = {
-	entry: {
-		'app': src + '/index.js',
-	},
-	output: {
-		path: dist,
-		filename: '[name].js',
-		chunkFilename: '[id].[name].js',
-		pathinfo: true
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel'
-			}
-		]
-	},
-	plugins: [
-		new CleanWebpackPlugin(dist),
-		//抽取a,b,c三个分离点chunk中公用的模块，本例抽取的是`babel-runtime/xx`和`core-js`公用模块，
-		//例如 ../~/core-js/library/modules/es6.object.define-property.js
-		//参考例子：https://github.com/webpack/webpack/tree/master/examples/extra-async-chunk
-		new webpack.optimize.CommonsChunkPlugin({
-			 name: 'app',
-			 async: true
-		}),
-		new htmlWebpackPlugin()
-	]
+  entry: {
+    'app': src + '/index.js',
+  },
+  output: {
+    path: dist,
+    filename: '[name].js',
+    chunkFilename: '[id].[name].js',
+    pathinfo: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(dist),
+    //抽取a,b,c三个分离点chunk中公用的模块，本例抽取的是`babel-runtime/xx`和`core-js`公用模块，
+    //例如 ../~/core-js/library/modules/es6.object.define-property.js
+    //参考例子：https://github.com/webpack/webpack/tree/master/examples/extra-async-chunk
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'app',
+      async: true,
+      // children: true,
+      filename: 'common.js',
+    }),
+    new htmlWebpackPlugin()
+  ]
 }
--- webpack2.x/react-router-v4/web/basic/app/modules/topics/main.js
+++ webpack2.x/react-router-v4/web/basic/app/modules/topics/main.js
@@ -3,22 +3,22 @@ import Link from 'react-router-dom/Link';
 import Route from 'react-router-dom/Route';
 import Topic from './components/Topic';
 
-export default ({match}) => (
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            <li>
-                <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/components`}>Components</Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-            </li>
-        </ul>
+export default ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
 
-        <Route path={`${match.url}/:topicId`} component={Topic}/>
-        <Route exact path={match.url} render={() => <h3>Please select a topic.</h3>}/>
-    </div>
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route exact path={match.url} render={() => <h3>Please select a topic.</h3>} />
+  </div>
 )
