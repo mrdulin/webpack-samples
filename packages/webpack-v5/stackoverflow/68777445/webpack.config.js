b4b14d2a8a4fb906f425edea6dde2ed1d907cc94
mrdulin <novaline.dulin@gmail.com>
2021-08-24 20:14:42 +0800

    feat: add example

---
+++ packages/webpack-v5/stackoverflow/68777445/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  plugins: [
+    new HTMLPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
