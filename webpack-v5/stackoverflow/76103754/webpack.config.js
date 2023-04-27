ddb94b2135e281c738fc68a5bad2957204676ee7
mrdulin <novaline.dulin@gmail.com>
2023-04-27 15:30:04 +0800

    feat: add html-webpack-plugin example

---
+++ webpack-v5/stackoverflow/76103754/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      // template: ['./index.html'],
+      template: './index.html'
+    })
+  ]
+};
---
+++ webpack-v5/template/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
+};
