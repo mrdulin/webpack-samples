c41e806072eda1a443a5071f3174f7ca388455e3
mrdulin <novaline.dulin@gmail.com>
2024-05-17 11:44:59 +0800

    feat: add css-loader style-loader example

---
+++ webpack-v5/stackoverflow/78488563/src/index.js
@@ -0,0 +1,2 @@
+import * as styles from './styles.module.css';
+console.log('🚀 ~ styles:', styles);
---
+++ webpack-v5/stackoverflow/78488563/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [
+					{ loader: 'style-loader' },
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [new HtmlWebpackPlugin({})],
+};
